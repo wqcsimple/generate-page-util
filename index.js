9f1b18f6dd7797d3e9ba6a31c67018d9fadd9450
whis <wqcsimple@foxmail.com>
2018-02-28 10:38:30 +0800

    fix export bug

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 
-module.exports = require('./generate_page'); // generate page
-module.exports = require('./generate_java_model');
+module.exports.generatePage = require('./generate_page'); // generate page
+module.exports.generateModel = require('./generate_java_model');
