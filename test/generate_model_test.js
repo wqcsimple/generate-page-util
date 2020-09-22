a7add7023c0973fee6b95e42d34b17497fec29fa
whis <>
2020-09-22 14:46:36 +0800

    [update] 1. 修改Model生成模板

--- test/generate_model_test.js
+++ test/generate_model_test.js
@@ -14,16 +14,19 @@ const generateModel = require('../index').generateModel;
 // };
 
 let dbConfig = {
-    host: "106.14.135.129",
-    user: "dev",
-    password: "rDbKU6Qy4XhwKpKl",
-    database: "phm_api",
-    table: ['user'],
-    writePath: `${__dirname}/ejs`
+  host: "106.14.135.129",
+  user: "dev",
+  password: "rDbKU6Qy4XhwKpKl",
+  database: "phm_api",
+  table: ['account', 'authority', 'device', 'device_action', 'device_event_action', 'device_fault_event',
+      'device_task', 'device_task_approval', 'device_task_member', 'device_warn_event', 'org', 'pay', 'pay_notification',
+      'role', 'role_authority', 'soft_update_log', 'subscribe_item', 'subscribe_item_link', 'subway_line', 'subway_station',
+      'sys_config', 'sys_menu', 'token', 'train', 'train_carriage', 'train_door_sensor', 'user', 'user_authority', 'user_bind',
+      'user_oper_log', 'user_role', 'vendor_component', 'vendor_vehicle'],
+  writePath: `${__dirname}/ejs`
 };
 
 generateModel(dbConfig);
-
 
 
 // let a = '1';
